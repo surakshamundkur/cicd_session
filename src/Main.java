d9e52d72255a563431364fe747a420c9441982a2
surakshamundkur <154885639+surakshamundkur@users.noreply.github.com>
2024-04-09 20:08:36 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 <<<<<<< HEAD
             String response = "Hello, this is a simple Java web application !";
 =======
-            String response = "Hello, this is a simple Java web application !";
+            String response = "Hello, this is a simple Java web application demo !";
 >>>>>>> e976f4c1ac7a1379b218cddcd24a0dcb922408aa
             exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
